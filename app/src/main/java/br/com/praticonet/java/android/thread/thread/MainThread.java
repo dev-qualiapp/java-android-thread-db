aa0aba5d149e08ca8b360069fe831446997642e3
Dev Qualiapp <dev.qualiapp@gmail.com>
2023-11-14 21:55:23 -0300

    starting implementing MainThread

--- app/src/main/java/br/com/praticonet/java/android/thread/thread/MainThread.java
+++ app/src/main/java/br/com/praticonet/java/android/thread/thread/MainThread.java
@@ -7,4 +7,33 @@ public class MainThread extends AsyncTask<String, String, Integer> {
     protected Integer doInBackground(String... strings) {
         return 0;
     }
+
+    public MainThread() {
+        super();
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        super.onPostExecute(integer);
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onCancelled(Integer integer) {
+        super.onCancelled(integer);
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+    }
 }
